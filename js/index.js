0508b349d16f6d501c7ae45860d10c253c4cea9c
Gabriel Osuobiem <osuobiem@gmail.com>
2021-07-13 18:49:19 +0100

    Add word and letter multipliers

--- js/index.js
+++ js/index.js
@@ -1,21 +1,42 @@
 $(document).ready(function () {
-    
-    // Make Cells
-    makeCells();
+	
+	// Make Cells
+	makeCells();
+
+	// Add Cell Multipliers
+	addMultipliers();
 });
 
 /**
  * Make cells
  */
 function makeCells() {
-    // Main board
-    mainBoard = $('#main-board');
+	// Main board
+	mainBoard = $('#main-board');
 
-    // Run loop to multiply cell
-    for(var i = 1; i <= 225; i++) {
-        cell = '<div class="cell" id="cell-'+i+'" draggable="true"></div>';
-        mainBoard.append(cell)
-    }
+	// Run loop to multiply cell
+	for(var i = 1; i <= 225; i++) {
+		cell = '<div class="cell" id="cell-'+i+'"></div>';
+		mainBoard.append(cell)
+	}
+}
 
-    $('#cell-112').append('<span class="letter">A</span><span class="point">1</span>').addClass('tile');
+/**
+ * Add cell multipliers
+ */
+function addMultipliers() {
+	// Double letter cells
+	multiplier = {
+		'double-letter':	[4, 12, 37, 39, 53, 46, 60, 93, 97, 99, 103, 109, 117, 123, 127, 129, 133, 166, 173, 180, 187, 189, 214, 222],
+		'triple-letter':	[21, 25, 81, 85, 141, 145, 201, 205],
+		'double-word':		[17, 33, 49, 65, 29, 43, 57, 71, 161, 177, 193, 209, 155, 169, 183, 197],
+		'triple-word':		[1, 8, 15, 106, 120, 211, 218, 225]
+	}
+	
+	// Multiply
+	for(type in multiplier) {
+		multiplier[type].forEach(cell => {
+			$('#cell-'+cell).addClass(type);
+		});
+	}
 }
