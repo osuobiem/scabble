9b030ca17b7a9e6589c77d66987a406777ac7cb0
Gabriel Osuobiem <osuobiem@gmail.com>
2021-07-13 17:35:49 +0100

    Add tile and letter style

--- js/index.js
+++ js/index.js
@@ -13,7 +13,9 @@ function makeCells() {
 
     // Run loop to multiply cell
     for(var i = 1; i <= 225; i++) {
-        cell = '<div class="cell" id="cell-'+i+'"></div>';
+        cell = '<div class="cell" id="cell-'+i+'" draggable="true"></div>';
         mainBoard.append(cell)
     }
+
+    $('#cell-112').append('<span class="letter">A</span><span class="point">1</span>').addClass('tile');
 }
